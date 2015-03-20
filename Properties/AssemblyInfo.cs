adfc70506dc1800bc6c0ad1453ecd4bfe94b62f0
Benton Stark <benton.stark@gmail.com>
2015-03-20 14:04:40 -0400

    Updates to project
    * removed biko name in assembly
    * bumped version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,19 +3,18 @@ using System.Runtime.CompilerServices;
 using System;
 
 
-[assembly: AssemblyTitle("Starksoft Biko")]
-[assembly: AssemblyDescription("Cryptographic Library")]
+[assembly: AssemblyTitle("Starksoft Aspen")]
+[assembly: AssemblyDescription("Cryptography Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Starksoft, LLC")]
-[assembly: AssemblyProduct("Starksoft Biko Library")]
+[assembly: AssemblyProduct("Starksoft Aspen Library")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 Benton Stark")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("")]
-
 
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: CLSCompliant(true)]
