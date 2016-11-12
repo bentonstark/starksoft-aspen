40df74f780e07c6072f647ffc7c26f046de22146
benton.stark@gmail.com <benton.stark@gmail.com>
2016-11-12 09:39:19 -0500

    bumped minor version number

--- Starksoft.Aspen/Properties/AssemblyInfo.cs
+++ Starksoft.Aspen/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System;
 [assembly: AssemblyCopyright("Copyright (c) 2016 Benton Stark")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("")]
 
