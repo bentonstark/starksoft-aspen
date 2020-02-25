98edd7e09b9d329e2c43a755421a01ad61340561
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2020-02-25 21:26:28 +0100

    Add demo to project

---
+++ Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Starksoft.Aspen.GnuPG;
+
+namespace Demo
+{
+    class Program
+    {
+        //project just 
+        static void Main(string[] args)
+        {
+            string keyId = "416AB815";
+            string passwd = "abc123!";
+            byte[] byteArray = Encoding.ASCII.GetBytes("Some text");
+            MemoryStream input = new MemoryStream(byteArray);
+            MemoryStream output = new MemoryStream();
+            Gpg gpg = new Gpg("C:\\Program Files (x86)\\GNU\\GnuPG\\pub\\gpg.exe");
+            gpg.OutputType = OutputTypes.AsciiArmor;
+            gpg.Passphrase = passwd;
+            gpg.LocalUser = keyId;
+            gpg.OutputSignatureType = OutputSignatureTypes.ClearText;
+            gpg.Sign(input, output);
+            output.Position = 0;
+            StreamReader reader = new StreamReader(output);
+            string text = reader.ReadToEnd();
+            MemoryStream outputVer = new MemoryStream();
+            gpg.Verify(output, outputVer);
+            outputVer.Position = 0;
+            StreamReader reader2 = new StreamReader(outputVer);
+            string text2 = reader2.ReadToEnd();
+            Console.ReadKey();
+        }
+    }
+}
